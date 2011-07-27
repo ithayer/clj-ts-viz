b98c4bf5d8421fbd33a65cb356633722accc4ed1
Paul Lam <paul@quantisan.com>
2011-07-27 19:32:03 -0400

    completed implementing k-means clustering

--- src/clj_ts_viz/model/cluster.clj
+++ src/clj_ts_viz/model/cluster.clj
@@ -1,15 +1,30 @@
 (ns clj-ts-viz.model.cluster  
-  (:use (clj-ts-viz.model qpart)))
+  (:use (clj-ts-viz.model kmeans qpart)))
 
+(comment
+  
 (defn add-cluster-col
   "Top level function that takes a dataset and returns a new dataset with a new column of cluster label"
   [coll n]
   (quantile-partition coll n))
 
-;; TODO split data into training and test sets for analysis
+) ; end of comment
+
+(defn- add-cluster-col
+  [f]
+  (fn [coll n]
+    (f coll n)))
+
+;; implementations
+(def add-kmeans-cluster (add-cluster-col kmeans-cluster))
+(def add-quantile-partition (add-cluster-col quantile-partition))
+
+;; TODO check correlation of different implementations with magic number or test set
 
 ;; testing only   
-;(require '[clj-ts-viz.data :as data])
-;(def data (data/gen-dataset))
-;(def a (add-cluster-col data 3))
-;(map :cluster a)
+(require '[clj-ts-viz.data :as data])
+(def data (data/gen-dataset))
+(def k (add-kmeans-cluster data 3))
+(map :cluster k)
+(def q (add-quantile-partition data 3))
+(map :cluster q)
--- src/clj_ts_viz/model/kmeans.clj
+++ src/clj_ts_viz/model/kmeans.clj
@@ -1,38 +1,72 @@
 (ns clj-ts-viz.model.kmeans
   "k-means clustering"
   (:use clj-ts-viz.model.process)
-  (:use [clojure.contrib.math :only (floor)])
-  (:use (incanter [stats :only (mean)])))
+  (:use (incanter [core :only (abs pow sqrt)]
+          				[stats :only (mean)])))
 
 (defn- random-parition
   "Randomly assign a cluster for initialization"
   [coll n]
   (let [[cell & more]		coll
-        rand-clus				(floor (rand n))]
-    (while (seq cell)
-      (cons (set-cluster rand-clus cell) (random-parition more n)))))
+        rand-clus				(int (rand n))]
+    (if (seq cell)
+      (cons (set-cluster rand-clus cell) (random-parition more n))
+      '())))
 
-(defn- cluster-mean
-  "Calculate the mean for a given cluster"
+;; TODO k-means on (i) slopes-only (done), (ii) slopes and means, (iii) slopes, means, volatility?
+
+(defrecord ClusterCentroid [id slopes-mean])
+
+(defn- cluster-centroid
+  "Calculate the centroid for a given cluster"
   [coll cluster]
   (let [subset	(filter #(= (:cluster %) cluster) coll)
-        slopes	(get-slopes subset)]		; TODO generalize
-    (mean slopes)))
+        slopes	(get-slopes subset)]		; TODO abstract out to generalize
+    (ClusterCentroid. cluster (mean slopes))))
 
-;; TODO (i) slopes-only, (ii) slopes and means, (iii) generalize 
+(defn- all-cluster-centroids
+  "Get centroid node for every cluster"
+  [coll]
+  (let [cluster-set		(set (map :cluster coll))]
+    (for [c	cluster-set]
+      (cluster-centroid coll c))))
 
-(comment
+(defn- euclid-distance
+  ([a b]					(abs (- a b)))
+  ([x1 y1 x2 y2]	(sqrt (+ (pow (- x1 x2) 2) 
+                         	 (pow (- y1 y2) 2)))))	; Eclidean distance
+
+(defn- distance-from-centroid
+  [cell cs-node]
+  (let [cell-slope		(get-slope-memo cell)		; TODO abstract out to generalize
+        cluster-slope	(:slopes-mean cs-node)]
+    (euclid-distance cell-slope cluster-slope)))
+
+(defn- closest-cluster
+  "Returns the closest cluster given a set of means"
+  [cell cc-nodes]
+	{:pre [(seq cc-nodes)]}
+ (:id (first (sort-by #(distance-from-centroid cell %) cc-nodes))))
+
+(defn- reset-to-closest-cluster  
+  [coll]
+  {:post [(= (count coll) (count %))]}
+  (let [cc-nodes		(all-cluster-centroids coll)]
+    (map #(set-cluster (closest-cluster % cc-nodes) %) coll)))
+
+(defn- update 
+  [coll]
+  (let [updated		(reset-to-closest-cluster	coll)]
+    (if (= coll updated)
+      updated				; convergence
+      (update updated))))
 
 (defn kmeans-cluster
-  "Takes a dataset and returns a new dataset with a new column of cluster label using quantile paritioning"
+  "Takes a dataset and returns a new dataset with a new column of cluster label using k-means"
   [coll n]
-  (let [slopes		(get-slopes coll)		; TODO abstract out to generalize
-        quantiles	(get-quantiles slopes n)]
-    (map (partial set-quantile-partition quantiles) coll)))
-
-) ; end of comment
+  (let [init	(random-parition coll n)]
+    (update init)))
 
 ;; testing only   
-(require '[clj-ts-viz.data :as data])
-(def data (data/gen-dataset))
-(def xs (get-slopes data))
+;(require '[clj-ts-viz.data :as data])
+;(def data (data/gen-dataset))
